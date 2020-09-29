ae7d5cc83743b693671732f69fb515227325ddba
Lucas Brito <lucasbsilva29@gmail.com>
2020-09-28 22:55:02 -0300

    feat(exercices.m): making bar, stem and plot graphs

--- homework_3/exercices.m
+++ homework_3/exercices.m
@@ -2,12 +2,31 @@ clear all
 close all
 
 # T_1
-f = imread('Figuras2/mammogram.bmp');
+img = imread('Figuras2/mammogram.bmp');
 imfinfo Figuras2/mammogram.bmp;
 figure(1)
 subplot(1,2,1);
-imshow(f);
+imshow(img);
 subplot(1,2,2);
-imhist(f);
+imhist(img);
 
 # E_1
+function my_histogram = MyHisto(img)
+  [h w] = size(img);
+  flat_image = img(:);
+  aux = zeros(256,1);
+  
+  for i = 1:256
+    aux(i,1) = sum(flat_image == i) / size(flat_image)(1);
+  endfor;
+  my_histogram = aux;
+endfunction;
+# hist = MyHisto(img);
+hist = imhist(img, 256);
+figure(2)
+subplot(1,3,1);
+bar(hist,0.1);
+subplot(1,3,2);
+stem(hist, "linewidth", 0.1);
+subplot(1,3,3);
+plot(hist)
