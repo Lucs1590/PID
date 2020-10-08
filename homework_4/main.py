4f2b7f4dfaf65c6af05eb72b99cec5deb1da1ee4
Lucas Brito <lucasbsilva29@gmail.com>
2020-10-07 21:47:31 -0300

    feat(main.py): read img and changing image color

--- homework_4/main.py
+++ homework_4/main.py
@@ -3,10 +3,27 @@ import cv2
 
 def run_pipeline():
     """
-    # Run Pipeline
+    ## Run Pipeline
     This function is the spine of project, so here all the functions are runned.
     """
-    pass
+    raw_img = read_img('')
+    gray_img = change_img_color(raw_img, cv2.COLOR_BGR2GRAY)
+
+
+def read_img(path):
+    """
+    ## Read Image
+    This function make the image reading.
+    """
+    return cv2.imread(path)
+
+
+def change_img_color(img, color_transformation):
+    """
+    ## Change Color Image
+    This function change the color of image.
+    """
+    return cv2.cvtColor(img, color_transformation)
 
 
 if __name__ == "__main__":
