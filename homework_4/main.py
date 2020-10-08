7e80b6d7fde4945cb0b52b21ae65d1972b7a58c3
Lucas Brito <lucasbsilva29@gmail.com>
2020-10-07 21:34:50 -0300

    feat(main.py): starting project with general hough transformation

---
+++ homework_4/main.py
@@ -0,0 +1,13 @@
+import cv2
+
+
+def run_pipeline():
+    """
+    # Run Pipeline
+    This function is the spine of project, so here all the functions are runned.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    run_pipeline()
