67a790e487eb0f90cff28bc792b157c1fdd158e0
Lucas Brito <lucasbsilva29@gmail.com>
2020-10-08 00:59:06 -0300

    feat(main.py): convolution and filters

--- homework_4/main.py
+++ homework_4/main.py
@@ -1,21 +1,23 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 def run_pipeline():
     """
-    ## Run Pipeline
+    # Run Pipeline
     This function is the spine of project, so here all the functions are runned.
     """
     raw_img = read_img('phone_0.png')
     gray_img = change_img_color(raw_img, cv2.COLOR_BGR2GRAY)
-    plt.imshow(raw_img)
+    edge_img = apply_filter(gray_img, 'sobel')
+    plt.imshow(edge_img)
     plt.show()
 
 
 def read_img(path):
     """
-    ## Read Image
+    # Read Image
     This function make the image reading.
     """
     return cv2.imread(path)
@@ -23,10 +25,57 @@ def read_img(path):
 
 def change_img_color(img, color_transformation):
     """
-    ## Change Color Image
+    # Change Color Image
     This function change the color of image.
     """
     return cv2.cvtColor(img, color_transformation)
+
+
+def apply_filter(img, name=''):
+    """
+    # Apply Filter
+    Select a filter to make convolution process.
+
+    By default we use roberts filter.
+    """
+    if name == 'sobel':
+        h_kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+        h_filtered_image = make_convolution(img, h_kernel)
+
+        v_kernel = np.array([[-1, -2, -1], [0, 0, 0], [-1, -2, -1]])
+        v_filtered_image = make_convolution(img, v_kernel)
+
+        aux_img = np.sum(h_filtered_image, v_filtered_image)
+    elif name == 'prewitt':
+        h_kernel = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+        h_filtered_image = make_convolution(img, h_kernel)
+
+        v_kernel = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
+        v_filtered_image = make_convolution(img, v_kernel)
+
+        aux_img = np.sum(h_filtered_image, v_filtered_image)
+    else:
+        h_kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+        h_filtered_image = make_convolution(img, h_kernel)
+
+        v_kernel = np.array([[-1, -2, -1], [0, 0, 0], [-1, -2, -1]])
+        v_filtered_image = make_convolution(img, v_kernel)
+
+        aux_img = np.sum(h_filtered_image, v_filtered_image)
+
+    return aux_img
+
+
+def make_convolution(img, kernel):
+    """
+    # Make Convolution
+    https://en.wikipedia.org/wiki/Convolution
+    """
+    aux = np.zeros_like(img)
+    for i in range(1, img.shape[0]-2):
+        for j in range(1, img.shape[1]-2):
+            aux = np.dot(img[i-1:i+2, j-1:j+2], kernel)
+    return aux
 
 
 if __name__ == "__main__":
