34a9fb169df4a4a4eda1cb5bd0fe0fee3c9fa455
Lucas Brito <lucasbsilva29@gmail.com>
2020-10-13 00:39:50 -0300

    feat(main.py): defining default r_table

--- homework_4/src/main.py
+++ homework_4/src/main.py
@@ -1,6 +1,8 @@
 import cv2
+import math
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 
 def run_pipeline():
@@ -8,13 +10,14 @@ def run_pipeline():
     # Run Pipeline
     This function is the spine of project, so here all the functions are runned.
     """
-    raw_img = read_img('img/phone_0.png')
+    raw_img = read_img('img/input_1.png')
     gray_img = change_img_color(raw_img, cv2.COLOR_BGR2GRAY)
     edge_img = apply_filter(gray_img, 'sobel')
+    # show_img(edge_img, 'cv')
+    r_table = build_r_table(edge_img)
     # find centroids
     # read test image
     # find object into image
-    # show_img(edge_img, 'cv')
 
 
 def read_img(path):
@@ -87,6 +90,47 @@ def show_img(img, param=''):
     else:
         plt.imshow(img)
         plt.show()
+
+
+def build_r_table(img):
+    """
+    # Build R Table
+    Building r table acording edge image
+    """
+    border_values = find_border_values(img)
+    r_table = define_default_r_table(len(border_values))
+    xc, yc = (img.shape[0]/2, img.shape[1]/2)
+    for values in border_values:
+        (x, y, value) = values
+        r = math.sqrt((x-xc)**2 + (y-yc)**2)
+        alpha = math.atan((y-yc)/(x-xc)) if x != xc else 0
+        # TODO: Calculate angle and add on r_table
+    return r_table
+
+
+def find_border_values(img):
+    """
+    # Find border values
+    Mapping all x, y and edge values of the image
+    """
+    coordinates = []
+    for i in range(0, img.shape[0]):
+        for j in range(1, img.shape[1]):
+            coordinates.append((i, j, img[i][j])) if img[i][j] != 0 else ...
+    return coordinates
+
+
+def define_default_r_table(k):
+    """
+    # Define Default R-Table
+    """
+    r_table = defaultdict(list)
+    gradient = 0
+    r_table[gradient]
+    while (gradient < 180):
+        gradient = round(gradient + 180/k, 2)
+        r_table[gradient]
+    return r_table
 
 
 if __name__ == "__main__":
