9d5d10b4879625d89311d69ef9a5939b3a97d7d5
Lucas Brito <lucasbsilva29@gmail.com>
2020-11-26 00:15:00 -0300

    feat(face.py): add new lbp

--- homework_5/face.py
+++ homework_5/face.py
@@ -13,7 +13,8 @@ from os import path
 from matplotlib import pyplot as plt
 # from google.colab.patches import cv2_imshow
 from mtcnn import MTCNN
-
+from skimage import feature
+from sklearn.svm import LinearSVC
 
 """ Main Function """
 
@@ -24,11 +25,16 @@ def main():
     print('Dataset verify')
     load_dataset(_path)
     _path = _path + '/arface'
+
     print('Run MTCNN')
-    faces_desc_mtcnn = detect_faces_mtcnn(
+    (faces_desc_mtcnn, labels_desc_mtcnn) = detect_faces_mtcnn(
         _path + '/face', _path + '/mtcnn_detect')
+
     print('Run LBP')
     faces_desc_lbp = run_lbp()
+    (faces_desc_lbp_2, labels_desc_lbp_2, model) = run_lbp_2(
+        _path + '/face', _path + '/lbp_detect')
+    # classify_lbp(_path + '/lbp_detect', model)
 
 
 """ Download Dataset """
@@ -43,6 +49,9 @@ def load_dataset(_path):
 
     if not path.isdir(destination + '/mtcnn_detect'):
         os.mkdir(destination + '/mtcnn_detect')
+
+    if not path.isdir(destination + '/lbp_detect'):
+        os.mkdir(destination + '/lbp_detect')
 
     if not path.isdir(destination + '/face'):
         get_dataset(dataset_file, destination)
@@ -78,20 +87,21 @@ def unzip_file(_file, destination):
 
 def detect_faces_mtcnn(_path, destination):
     faces = []
-    label = []
+    labels = []
 
-    for arquivo in glob.glob(path.join(_path, "*.bmp")):
-        img = cv2.cvtColor(cv2.imread(arquivo), cv2.COLOR_BGR2RGB)
+    for _file in glob.glob(path.join(_path, "*.bmp")):
+        img = cv2.cvtColor(cv2.imread(_file), cv2.COLOR_BGR2RGB)
         detected_face = MTCNN().detect_faces(img)
         if detected_face:
-            label.append(arquivo.split('/')[-1])
+            labels.append(_file.split(os.path.sep)[-1].split('-')[1])
             faces.append(detected_face)
-            copy_file(arquivo, destination)
+            copy_file(_file, destination)
 
-    detected = list(map(lambda aqv: aqv.split('/')[-1].split('-')[-1].split('_')[0],glob.glob(path.join(destination, "*.bmp"))))
-    count_detected = {i:detected.count(i) for i in detected}
+    detected = list(map(lambda aqv: aqv.split(
+        os.path.sep)[-1].split('-')[-1].split('_')[0], glob.glob(path.join(destination, "*.bmp"))))
+    count_detected = {i: detected.count(i) for i in detected}
     print(count_detected)
-    return faces
+    return faces, labels
 
 
 def copy_file(file, destination):
@@ -102,11 +112,66 @@ def copy_file(file, destination):
 """Local Binary Pattern (LBP)"""
 
 
+class LocalBinaryPatterns:
+    def __init__(self, num_points, radius):
+        self.numPoints = num_points
+        self.radius = radius
+
+    def describe(self, image, eps=1e-5):
+        lbp = feature.local_binary_pattern(image, self.numPoints,
+                                           self.radius, method="uniform")
+        (hist, _) = np.histogram(lbp.ravel(),
+                                 bins=np.arange(0, self.numPoints + 3),
+                                 range=(0, self.numPoints + 2))
+
+        hist = hist.astype("float")
+        hist /= (hist.sum() + eps)
+
+        return hist
+
+
+def run_lbp_2(_path, destination):
+    desc = LocalBinaryPatterns(24, 8)
+    faces = []
+    labels = []
+
+    # train
+    for _file in glob.glob(path.join(_path, "*.bmp")):
+        gray_img = cv2.cvtColor(cv2.imread(_file), cv2.COLOR_BGR2GRAY)
+        hist = desc.describe(gray_img)
+
+        if hist:
+            labels.append(_file.split(os.path.sep)[-1].split('-')[1])
+            faces.append(hist)
+            copy_file(_file, destination)
+
+    model = LinearSVC(C=100.0, random_state=42)
+    model.fit(faces, labels)
+
+    return faces, labels, model
+
+
+def classify_lbp(_path, model):
+    desc = LocalBinaryPatterns(24, 8)
+
+    for _file in glob.glob(path.join(_path, "*.bmp")):
+        image = cv2.imread(_file)
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        hist = desc.describe(gray_img)
+        prediction = model.predict(hist.reshape(1, -1))
+
+        # display the image and the prediction
+        cv2.putText(image, prediction[0], (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                    1.0, (0, 0, 255), 3)
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+
+
 def run_lbp(_path):
     images = []
 
-    for arquivo in glob.glob(path.join(_path, "*.bmp")):
-        img = cv2.cvtColor(cv2.imread(arquivo), cv2.COLOR_BGR2RGB)
+    for _file in glob.glob(path.join(_path, "*.bmp")):
+        img = cv2.cvtColor(cv2.imread(_file), cv2.COLOR_BGR2RGB)
         img_gray = cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2GRAY)
         height, width = img_gray.shape
         img_lbp = np.zeros((height, width), np.uint8)
