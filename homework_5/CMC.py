a31c76b1aa22a73a9f5707ba64e74c7229a1c1ba
Lucas Brito <lucasbsilva29@gmail.com>
2020-12-17 21:21:05 -0300

    style(CMC.py): indentação

--- homework_5/CMC.py
+++ homework_5/CMC.py
@@ -2,16 +2,17 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
-default_color = ['r','g','b','c','m','y','orange','brown']
-default_marker = ['*','o','s','v','X','*','.','P']
+default_color = ['r', 'g', 'b', 'c', 'm', 'y', 'orange', 'brown']
+default_marker = ['*', 'o', 's', 'v', 'X', '*', '.', 'P']
+
 
 class CMC:
-    def __init__(self,cmc_dict, color=default_color, marker = default_marker):
+    def __init__(self, cmc_dict, color=default_color, marker=default_marker):
         self.color = color
         self.marker = marker
         self.cmc_dict = cmc_dict
-        
-    def plot(self,title,rank=20, xlabel='Rank',ylabel='Matching Rates (%)',show_grid=True):        
+
+    def plot(self, title, rank=20, xlabel='Rank', ylabel='Matching Rates (%)', show_grid=True):
         fig, ax = plt.subplots()
         fig.suptitle(title)
         x = list(range(0, rank+1, 5))
@@ -31,19 +32,22 @@ class CMC:
             else:
                 temp_cmc = self.cmc_dict[name]
                 r = list(range(1, len(temp_cmc)+1))
-                
+
             if name == list(self.cmc_dict.keys())[-1]:
-                globals()[name] = mlines.Line2D(r, temp_cmc, color=self.color[0], marker=self.marker[0], label='{:.1f}% {}'.format(self.cmc_dict[name][0]*100, name))
+
+                globals()[name] = mlines.Line2D(r, temp_cmc, color=self.color[0],
+                                                marker=self.marker[0], label='{:.1f}% {}'.format(self.cmc_dict[name][0]*100, name))
             else:
-                globals()[name] = mlines.Line2D(r, temp_cmc, color=self.color[i+1], marker=self.marker[i+1], label='{:.1f}% {}'.format(self.cmc_dict[name][0]*100, name))
+                globals()[name] = mlines.Line2D(r, temp_cmc, color=self.color[i+1],
+                                                marker=self.marker[i+1], label='{:.1f}% {}'.format(self.cmc_dict[name][0]*100, name))
                 i = i+1
             ax.add_line(globals()[name])
             method_name.append(globals()[name])
 
         plt.legend(handles=method_name)
         plt.show()
-    
-    def save(self, title, filename, 
+
+    def save(self, title, filename,
              rank=20, xlabel='Rank',
              ylabel='Matching Rates (%)', show_grid=True,
              save_path=os.getcwd(), format='png', **kwargs):
@@ -66,17 +70,19 @@ class CMC:
             else:
                 temp_cmc = self.cmc_dict[name]
                 r = list(range(1, len(temp_cmc)+1))
-                
+
             if name == list(self.cmc_dict.keys())[-1]:
-                globals()[name] = mlines.Line2D(r, temp_cmc, color='r', marker='*', label='{:.1f}% {}'.format(self.cmc_dict[name][0]*100, name))
+                globals()[name] = mlines.Line2D(r, temp_cmc, color='r', marker='*',
+                                                label='{:.1f}% {}'.format(self.cmc_dict[name][0]*100, name))
             else:
-                globals()[name] = mlines.Line2D(r, temp_cmc, color=self.color[i], marker=self.marker[i], label='{:.1f}% {}'.format(self.cmc_dict[name][0]*100, name))
+                globals()[name] = mlines.Line2D(r, temp_cmc, color=self.color[i],
+                                                marker=self.marker[i], label='{:.1f}% {}'.format(self.cmc_dict[name][0]*100, name))
                 i = i+1
             ax.add_line(globals()[name])
             method_name.append(globals()[name])
 
         plt.legend(handles=method_name)
-        fig.savefig(os.path.join(save_path,filename+'.'+format), 
+        fig.savefig(os.path.join(save_path, filename+'.'+format),
                     format=format,
                     bbox_inches='tight',
-                    pad_inches = 0, **kwargs)
+                    pad_inches=0, **kwargs)
